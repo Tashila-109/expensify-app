cf5b8008475f7992f9623d3ce2545f065ccf7d36
Tashila Fernando <tashila97@hotmail.com>
2019-09-21 19:49:53 +0530

    Setup Database and env

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 //entry -> output
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+	require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = env => {
 	const isProduction = env === 'production';
 	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
 
 	return {
-		mode: 'development',
 		entry: './src/app.js',
 		output: {
 			path: path.join(__dirname, 'public', 'dist'),
@@ -45,7 +53,30 @@ module.exports = env => {
 			]
 		},
 		plugins: [
-			CSSExtract
+			CSSExtract,
+			new webpack.DefinePlugin({
+				'process.env.FIREBASE_API_KEY': JSON.stringify(
+					process.env.FIREBASE_API_KEY
+				),
+				'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
+					process.env.FIREBASE_AUTH_DOMAIN
+				),
+				'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
+					process.env.FIREBASE_DATABASE_URL
+				),
+				'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+					process.env.FIREBASE_PROJECT_ID
+				),
+				'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
+					process.env.FIREBASE_API_KEY
+				),
+				'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+					process.env.FIREBASE_API_KEY
+				),
+				'process.env.FIREBASE_APP_ID': JSON.stringify(
+					process.env.FIREBASE_APP_ID
+				)
+			})
 		],
 		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
 		devServer: {
