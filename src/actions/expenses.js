b7c3c6d5fb7b490002c19a1cb42f2787e2ffd62c
Tashila Fernando <tashila97@hotmail.com>
2019-09-21 22:41:45 +0530

    Set, remove and edit expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -2,7 +2,7 @@ import uuid from 'uuid';
 import database from '../firebase/firebase';
 
 // ADD_EXPENSE
-export const addExpense = (expense) => ({
+export const addExpense = expense => ({
 	type: 'ADD_EXPENSE',
 	expense
 });
@@ -16,14 +16,19 @@ export const startAddExpense = (expenseData = {}) => {
 			createdAt = 0
 		} = expenseData;
 
-		const expense = { description, note, amount, createdAt }
+		const expense = { description, note, amount, createdAt };
 
-		database.ref('expenses').push(expense).then((ref) => {
-			dispatch(addExpense({
-				id: ref.key,
-				...expense
-			}))
-		})
+		database
+			.ref('expenses')
+			.push(expense)
+			.then(ref => {
+				dispatch(
+					addExpense({
+						id: ref.key,
+						...expense
+					})
+				);
+			});
 	};
 };
 
@@ -33,9 +38,56 @@ export const removeExpense = ({ id } = {}) => ({
 	id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+	return dispatch => {
+		return database
+			.ref(`expenses/${id}`)
+			.remove()
+			.then(() => {
+				dispatch(removeExpense({ id }));
+			});
+	};
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
 	type: 'EDIT_EXPENSE',
 	id,
 	updates
 });
+
+export const startEditExpense = (id, updates) => {
+	return dispatch => {
+		return database
+			.ref(`expenses/${id}`)
+			.update(updates)
+			.then(() => {
+				dispatch(editExpense(id, updates));
+			});
+	};
+};
+
+// FETCHING DATA - SET_EXPENSES
+export const setExpenses = expenses => ({
+	type: 'SET_EXPENSES',
+	expenses
+});
+
+export const startSetExpenses = () => {
+	return dispatch => {
+		return database
+			.ref('expenses')
+			.once('value')
+			.then(snapshot => {
+				const expenses = [];
+
+				snapshot.forEach(childSnapshot => {
+					expenses.push({
+						id: childSnapshot.key,
+						...childSnapshot.val()
+					});
+				});
+				dispatch(setExpenses(expenses));
+			});
+	};
+};
